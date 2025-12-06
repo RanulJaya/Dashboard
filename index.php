41ec07f445ba0f91e52e942dc0f5ceea425b87ae
RanulJaya <ranul.jaya@gmail.com>
2025-12-07 01:22:25 +1300

    modified index.php file

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
+	// if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+	// 	$uri = 'https://';
+	// } else {
+	// 	$uri = 'http://';
+	// }
+	$uri = 'https://'
 	$uri .= $_SERVER['HTTP_HOST'];
 	header('Location: '.$uri.'/components/');
 	exit;
