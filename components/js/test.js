0cb12cd2ec992c7af9fe07196532d814f9528505
RanulJaya <ranul.jaya@gmail.com>
2025-12-09 23:31:56 +1300

    Array created and using mulitple boxes to move around the area

--- components/js/test.js
+++ components/js/test.js
@@ -1,15 +1,43 @@
+for (let index = 0; index < 1; index++) {
+    var parent = document.getElementById('app')
+    var board = document.createElement("div")
+
+    board.id = 'cardGrab1'
+    board.className = 'card'
+    board.style.backgroundColor = 'rgba(223, 223, 89, 1)'
+    board.style.top = '200px'
+    board.style.left = '0px'
+    board.style.position = 'fixed'
+
+    var childBoard = document.createElement('div')
+    childBoard.className = 'card-body'
+
+    board.appendChild(childBoard)
+    parent.appendChild(board)
+}
+
+
+const xmlhttp = new XMLHttpRequest();
+
+xmlhttp.open("GET", "../jsonParser.php");
+xmlhttp.send();
+
+xmlhttp.onload = function() {
+  const myObj = JSON.parse(this.responseText);
+  document.getElementById("demo").innerHTML = myObj[1]['body'];
+}
+
 let newX = 0, newY = 0, startX = 0, startY = 0;
 
-const card = document.getElementById("cardGrab")
-const textArea = document.getElementById("text")
-const cardBody = document.querySelector(".card-body")
-
-
-  console.log(cardBody.style)
+const card = [document.getElementById("cardGrab"), document.getElementById("cardGrab1")]
 
 // Initiate drag from adding listener
-card.addEventListener('mousedown', mouseDown)
-card.addEventListener('keypress', keyPress)
+
+card[0].addEventListener('mousedown', mouseDown)
+card[1].addEventListener('mousedown', mouseDown)
+
+console.log(card[1])
+
 
 function mouseDown(e){
     // Start point of the coordinates
@@ -18,41 +46,29 @@ function mouseDown(e){
 
     document.addEventListener('mousemove', mouseMove)
     document.addEventListener('mouseup', mouseUp)
+
 }
 
 function mouseMove(e){
     newX = startX - e.clientX 
     newY = startY - e.clientY 
-  
+
     startX = e.clientX
     startY = e.clientY
+    
+    console.log(e.target.id)
 
-    card.style.top = (card.offsetTop - newY) + 'px'
-    card.style.left = (card.offsetLeft - newX) + 'px'
-
+    for (let index = 0; index < 2; index++) {
+        if (card[index].id == e.target.id) {
+          card[index].style.top = (card[index].offsetTop - newY) + 'px'
+          card[index].style.left = (card[index].offsetLeft- newX) + 'px'
+        }
+    }
 }
 
 function mouseUp(e){
     document.removeEventListener('mousemove', mouseMove)
 }
-
-function keyPress(e){
-
-  if(card.clientHeight > 200){
-    card.style.height = "200px"
-    textArea.style.height = "175px"
-    textArea.style.overflowY = "visible"
-  }
-}
-
-const xmlhttp = new XMLHttpRequest();
-
-xmlhttp.onload = function() {
-  const myObj = JSON.parse(this.responseText);
-  document.getElementById("demo").innerHTML = myObj[1]['body'];
-}
-xmlhttp.open("GET", "../jsonParser.php");
-xmlhttp.send();
 
 
             
