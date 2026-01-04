92667d80a85a06f084f9a9828a7bcf2b5b4d015d
RanulJaya <ranul.jaya@gmail.com>
2026-01-04 15:52:28 +1300

    hashed sign in and fixed sign on

--- connectToDatabase.php
+++ connectToDatabase.php
@@ -2,25 +2,28 @@
         require __DIR__ . '/vendor/autoload.php';
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
         $dotenv->load();
-
         $conn = new mysqli(getenv('SERVER_NAME'), $_ENV['USERNAME'], $_ENV['PASSWRD'], $_ENV['SCHEMA']);
 
-        $sql = "SELECT * FROM userdata";
+        $sql = "SELECT * FROM users";
         $result = $conn->query($sql);
+
 
         $data = [];
         
           while($row = $result->fetch_assoc()) {
             $data[] = json_encode($row);
           }
-
           
+        
+        $password = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+
           if (isset($_POST["username"])) {
             # code...
             for ($x = 0; $x < count($data); $x++){
-
               $value = json_decode($data[$x], true);
-              if ($value['username'] == $_POST["username"] && $value['password'] == $_POST["pass"]) {
+
+              if ($value['username'] == $_POST["username"] && password_verify($value['password'], $password)){
                 if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS']))  {
                   # code...
                   $uri = 'https://';
@@ -31,9 +34,11 @@
                 $uri .= $_SERVER['HTTP_HOST'];
                   // header("Access-Control-Allow-Origin: *");
                 header('Location: '.$uri.'/components/index.html');
-                exit;
                 }
             }
             echo "cannot connect";
+        }
+        else{
+          echo "Please enter in Username";
         }
      ?>
